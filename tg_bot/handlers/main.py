309d7f33b9c577511b1a14405064b1520c74d932
sdiki1 <volodymyrbuharin@yandex.ru>
2023-12-27 16:46:24 +0300

    codestyle

--- tg_bot/handlers/main.py
+++ tg_bot/handlers/main.py
@@ -13,7 +13,6 @@ async def start_handler(message: types.Message):
     print(message)
 
 
-    
 async def add_sound_handler(message: types.message, state: FSMContext):
     # print(message)
     name = message.text[11:]
@@ -44,8 +43,8 @@ async def get_audio(message: types.Document, state: FSMContext):
         local_file_path = f"files/{message.from_user.id}/{file_name}"
         await file.download(destination=local_file_path)
 
-        input_file = local_file_path 
-        output_file = local_file_path[:-3]+'oga' 
+        input_file = local_file_path
+        output_file = local_file_path[:-3] + "oga"
 
         audio = AudioSegment.from_mp3(input_file)
         audio.export(output_file, format="oga")
@@ -54,9 +53,7 @@ async def get_audio(message: types.Document, state: FSMContext):
         Session = sessionmaker()
         session = Session(bind=engine)
         new_file = Files(
-            Id_user=message.from_user.id,
-            Name_sound=name,
-            Path=output_file
+            Id_user=message.from_user.id, Name_sound=name, Path=output_file
         )
         session.add(new_file)
         session.commit()
@@ -97,11 +94,12 @@ async def delete_message(message: types.message):
             "Didn't find file assosiated with entered <sound_name> in my storage! ❌😲"
         )
 
+
 async def upload_voice_and_get_link(bot, path):
     voice_path = path
 
-    with open(voice_path, 'rb') as voice_file:
-        voice_message = await bot.send_voice(chat_id='1924535035', voice=voice_file)
+    with open(voice_path, "rb") as voice_file:
+        voice_message = await bot.send_voice(chat_id="1924535035", voice=voice_file)
         return voice_message.voice.file_id
 
 
@@ -111,7 +109,12 @@ async def inline_echo(inline_query: types.InlineQuery):
     Session = sessionmaker()
     session = Session(bind=engine)
     today = datetime.datetime.today()
-    last_files = session.query(Files).filter(Files.Name_sound.like(f"{inline_query.query}%")).filter(Files.Date_genering_id <= (today - datetime.timedelta(days=30))).all()
+    last_files = (
+        session.query(Files)
+        .filter(Files.Name_sound.like(f"{inline_query.query}%"))
+        .filter(Files.Date_genering_id <= (today - datetime.timedelta(days=30)))
+        .all()
+    )
     for i in last_files:
         file_id = await upload_voice_and_get_link(inline_query.bot, i.Path)
         i.Id_audio = file_id
@@ -119,8 +122,12 @@ async def inline_echo(inline_query: types.InlineQuery):
     session.add_all(last_files)
     session.commit()
 
-
-    files = session.query(Files).filter(Files.Id_user == inline_query.from_user.id).filter(Files.Name_sound.like(f"{inline_query.query}%")).all()
+    files = (
+        session.query(Files)
+        .filter(Files.Id_user == inline_query.from_user.id)
+        .filter(Files.Name_sound.like(f"{inline_query.query}%"))
+        .all()
+    )
     for i in files:
         if i.Id_audio is None:
             file_id = await upload_voice_and_get_link(inline_query.bot, i.Path)
@@ -129,10 +136,17 @@ async def inline_echo(inline_query: types.InlineQuery):
     session.add_all(files)
     session.commit()
 
-    files =  session.query(Files).filter(and_(Files.Id_user == inline_query.from_user.id, Files.Name_sound.ilike(f"{inline_query.query}%"))).all()
+    files = (
+        session.query(Files)
+        .filter(
+            and_(
+                Files.Id_user == inline_query.from_user.id,
+                Files.Name_sound.ilike(f"{inline_query.query}%"),
+            )
+        )
+        .all()
+    )
     session.close()
-
-
 
     res = []
     for i in files:
@@ -142,15 +156,36 @@ async def inline_echo(inline_query: types.InlineQuery):
             voice_file_id=i.Id_audio,
         )
         res.append(voice)
-    if inline_query.query == '':
-        await inline_query.bot.answer_inline_query(inline_query.id, results=res, cache_time=1, switch_pm_text=f"All sounds:", switch_pm_parameter="start",
-                                  is_personal=False, next_offset="")
+    if inline_query.query == "":
+        await inline_query.bot.answer_inline_query(
+            inline_query.id,
+            results=res,
+            cache_time=1,
+            switch_pm_text=f"All sounds:",
+            switch_pm_parameter="start",
+            is_personal=False,
+            next_offset="",
+        )
     if res == []:
-        await inline_query.bot.answer_inline_query(inline_query.id, results=res, cache_time=1, switch_pm_text=f"Didn't find file assosiated with entered {inline_query.query} in my storage! ❌😲", switch_pm_parameter=f"start",
-                                  is_personal=False, next_offset="")
+        await inline_query.bot.answer_inline_query(
+            inline_query.id,
+            results=res,
+            cache_time=1,
+            switch_pm_text=f"Didn't find file assosiated with entered {inline_query.query} in my storage! ❌😲",
+            switch_pm_parameter=f"start",
+            is_personal=False,
+            next_offset="",
+        )
     else:
-        await inline_query.bot.answer_inline_query(inline_query.id, results=res, cache_time=1, switch_pm_text=f"All sounds, that name starts with: {inline_query.query}", switch_pm_parameter="start",
-                                  is_personal=True, next_offset="")
+        await inline_query.bot.answer_inline_query(
+            inline_query.id,
+            results=res,
+            cache_time=1,
+            switch_pm_text=f"All sounds, that name starts with: {inline_query.query}",
+            switch_pm_parameter="start",
+            is_personal=True,
+            next_offset="",
+        )
 
 
 def register_main_handlers(dp: dispatcher):
@@ -163,4 +198,3 @@ def register_main_handlers(dp: dispatcher):
     dp.register_inline_handler(inline_echo)
 
     # dp.on(types.Document, get_audio)
-    
