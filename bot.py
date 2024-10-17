08b3b4417aef6ad5f03d126c288cac7925ad4224
Ilya Savitsky <ipsavitsky234@gmail.com>
2024-10-17 18:33:33 +0000

    Modify handlers code to run on aiogram3

--- bot.py
+++ bot.py
@@ -9,37 +9,32 @@ import tg_bot.models.DBSM
 
 logger = logging.getLogger(__name__)
 
-
 def register_all_filters(dp):
     return None
 
-
 def register_all_handlers(dp):
     register_main_handlers(dp)
-
 
 async def main():
     logging.basicConfig(
         level=logging.INFO,
         format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
     )
+
     config = load_config(".env")
 
     bot = Bot(token=config.tg_bot.token)
     storage = MemoryStorage()
-    dp = Dispatcher(bot, storage=storage)
-    bot["config"] = config
+    dp = Dispatcher(storage=storage)
 
     register_all_filters(dp)
     register_all_handlers(dp)
 
     try:
-        await dp.start_polling()
+        await dp.start_polling(bot)
     finally:
         await dp.storage.close()
-        await dp.storage.wait_closed()
         await bot.session.close()
-
 
 if __name__ == "__main__":
     try:
